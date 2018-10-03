ba83a82b102c24e2f61715e1d792faa4c864b780
alvin <alvinyeats@gmail.com>
2018-10-03 11:22:41 +0800

    add chatper1

---
+++ src/chapter1/PoolPuzzleOne.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class PoolPuzzleOne {
+    public static void main(String [] args) {
+        int x = 0;
+
+        while (x < 4) {
+            if (x < 1) {
+                System.out.print("a");
+            }
+            System.out.print(" ");
+        }
+    }
+
+}
